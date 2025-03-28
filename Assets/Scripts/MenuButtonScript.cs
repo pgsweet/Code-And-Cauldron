4a8ceaf5c7a66d1026289749ab4d0cca03a5f89b
pgsweet <113042887+pgsweet@users.noreply.github.com>
2025-03-28 15:36:21 -0400

    added basic text input

--- Assets/Scripts/MenuButtonScript.cs
+++ Assets/Scripts/MenuButtonScript.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class MenuButtonScript : MonoBehaviour
 {
+    private GameObject editor;
+
+    void Start()
+    {
+        editor = GameObject.FindGameObjectWithTag("Editor");
+    }
     public void openEditor()
     {
         // get the editor from the tag and call the open editor function
-        GameObject editor = GameObject.FindGameObjectWithTag("Editor");
         if (editor == null)
         {
             Debug.LogError("Editor not found.");
             return;
         }
         editor.GetComponent<EditorScript>().toggleEditor();
+    }
+
+    public void runCode()
+    {
+        if (editor == null)
+        {
+            Debug.LogError("Editor not found.");
+            return;
+        }
+        editor.GetComponent<EditorScript>().runCode();
     }
 }
