f973bebd4db93f464f55638079e7b5071e69d543
pgsweet <113042887+pgsweet@users.noreply.github.com>
2025-04-03 14:15:53 -0400

    error checked most commands

--- Assets/Scripts/EditorScript.cs
+++ Assets/Scripts/EditorScript.cs
@@ -44,6 +44,11 @@ public class EditorScript : MonoBehaviour
         new string[] {"out", "NON_EMPTY", "NUMBER ALL"}
     };
 
+    // Formatted as [name, number of items, commands till expires]
+    public List<System.Object[]> inputItems = new List<System.Object[]>{
+        new System.Object[] {"test", 3, -1}
+    };
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -168,6 +173,22 @@ public class EditorScript : MonoBehaviour
                     Debug.LogError("Unknown command: " + line[0]);
                     break;
             }
+            // decrement the input items lifespan and notify the user if they expire
+            for (int i = 0; i < inputItems.Count(); i++) 
+            {
+                if ((int)inputItems[i][2] == -1)
+                {
+                    continue;
+                }
+                inputItems[i][2] = (int)inputItems[i][2] - 1;
+                if ((int)inputItems[i][2] == 0)
+                {
+                    Debug.LogError("Item " + inputItems[i][0] + " has expired.");
+                    inputItems.RemoveAt(i);
+                    i--;
+                }
+            }
+
             // Debug script
             // Debug.Log(string.Join(",", line));
         }
@@ -234,8 +255,6 @@ public class EditorScript : MonoBehaviour
             return;
         }
 
-
-
         // move items 
         int numItemsToMove = Int32.Parse(command[3]);
         string itemName = containers[container1Number][0].ToString();
@@ -263,31 +282,182 @@ public class EditorScript : MonoBehaviour
 
     private void botCommand(List<string> command)
     {
-        Debug.LogError("BOT command not implemented.");
+        if (command.Count() < 2)
+        {
+            Debug.LogError("BOT command requires at least 2 arguments.");
+            return;
+        }
+        if (command.Count() > 3)
+        {
+            Debug.LogError("BOT command accepts a maximum of 3 arguments.");
+            return;
+        }
+        // make sure arg1 is a valid container
+        int container1Number = -1;
+        bool arg1HasNum = Int32.TryParse(command[1].Substring(command[1].Length-1), out container1Number);
+        container1Number -= 1; // Adjust for zero-based index
+        if (!(command[1].Substring(0,command[1].Length-1) == "container" && arg1HasNum && container1Number >= 0 && container1Number <= 3))
+        {
+            Debug.LogError("Must have a valid container in the first argument.");
+            return;
+        }
+
+        // make sure arg1 is an empty container
+        if ((int)containers[container1Number][1] != 0)
+        {
+            Debug.LogError($"Container{container1Number} is not empty.");
+            return;
+        }
+
+        // check if user inputed a number of potions to bottle
+        if (command.Count() == 2)
+        {
+            // add the number of items in arg1 to arg3
+            command.Add("1");
+        }
+
+        // TODO: check if cauldron contains valid a valid recipie
+
+        // TODO: create the potions and place them into arg1
+        Debug.LogError("BOT command not finsihed.");
         return;
     }
 
     private void splCommand(List<string> command)
     {
+        // TODO: check if the cauldron has a valid spell
+
+        // TODO: Cast spell and remove items from cauldron
         Debug.LogError("SPL command not implemented.");
         return;
     }
 
     private void clrCommand(List<string> command)
     {
-        Debug.LogError("CLR command not implemented.");
+        // check if the command has exactly 2 arguments
+        if (command.Count() != 2)
+        {
+            Debug.LogError("CLR command requires exactly 2 arguments.");
+            return;
+        }
+
+        // make sure arg1 is a valid container or cauldron
+        int container1Number = -1;
+        bool arg2HasNum = Int32.TryParse(command[2].Substring(command[2].Length-1), out container1Number);
+        container1Number -= 1; // Adjust for zero-based index
+        if (!(command[2].Substring(0,command[2].Length-1) == "container" && arg2HasNum && container1Number >= 0 && container1Number <= 3) &&
+        !(command[2] == "cauldron"))
+        {
+            Debug.LogError("Must have a container or cauldron in the second argument.");
+            return;
+        }
+
+        // make sure arg1 has items
+        if (command[1] == "cauldron" && cauldron.Count() == 0)
+        {
+            Debug.LogError("Cauldron is already empty.");
+            return;
+        }
+        else if (command[1] != "cauldron" && (int)containers[container1Number][1] == 0)
+        {
+            Debug.LogError($"Container {container1Number} is already empty.");
+            return;
+        }
+
+        // TODO: clear arg1
+        Debug.LogError("CLR command not finished.");
         return;
     }
 
     private void inpCommand(List<string> command)
     {
-        Debug.LogError("INP command not implemented.");
+        // check if theres 1 argument
+        if (command.Count() != 2)
+        {
+            Debug.LogError("INP command requires exactly 1 argument.");
+            return;
+        }
+
+        // make sure arg1 is a valid container
+        int container1Number = -1;
+        bool arg1HasNum = Int32.TryParse(command[1].Substring(command[1].Length-1), out container1Number);
+        container1Number -= 1; // Adjust for zero-based index
+        if (!(command[1].Substring(0,command[1].Length-1) == "container" && arg1HasNum && container1Number >= 0 && container1Number <= 3))
+        {
+            Debug.LogError("Must have a valid container in the first argument.");
+            return;
+        }
+
+        // make sure theres an item to input
+        if (inputItems.Count() == 0)
+        {
+            Debug.LogError("No items to input.");
+            return;
+        }
+
+        // make sure arg1 is either empty or has the same item
+        if ((int)containers[container1Number][1] != 0 && containers[container1Number][0].ToString() != inputItems[0][0].ToString())
+        {
+            Debug.LogError($"Container {container1Number} is not empty or does not contain a similar item.");
+            return;
+        }
+
+        // pop the first input item and add it to the container
+        string itemName = inputItems[0][0].ToString();
+        int itemAmount = (int)inputItems[0][1];
+        containers[container1Number][0] = itemName;
+        containers[container1Number][1] = (int)containers[container1Number][1] + itemAmount;
+        inputItems.RemoveAt(0);
+
+        Debug.Log("inp command ran: " + command[1] + ", " + itemName + ", " + itemAmount);
         return;
     }
 
     private void outCommand(List<string> command)
     {
-        Debug.LogError("OUT command not implemented.");
+        // check if has at least 1 argument
+        if (command.Count() < 2)
+        {
+            Debug.LogError("OUT command requires at least 1 argument.");
+            return;
+        }
+        // check if has at most 2 arguments
+        if (command.Count() > 3)
+        {
+            Debug.LogError("OUT command accepts a maximum of 2 arguments.");
+            return;
+        }
+        // make sure arg1 is a valid container
+        int container1Number = -1;
+        bool arg1HasNum = Int32.TryParse(command[1].Substring(command[1].Length-1), out container1Number);
+        container1Number -= 1; // Adjust for zero-based index
+        if (!(command[1].Substring(0,command[1].Length-1) == "container" && arg1HasNum && container1Number >= 0 && container1Number <= 3))
+        {
+            Debug.LogError("Must have a valid container in the first argument.");
+            return;
+        }
+
+        // make sure arg1 is not empty
+        if ((int)containers[container1Number][1] == 0)
+        {
+            Debug.LogError($"Container {container1Number} is empty.");
+            return;
+        }
+        // check if user inputed a number of items to output
+        if (command.Count() == 2)
+        {
+            // add the number of items in arg1 to arg3
+            command.Add("1");
+        }
+        // check if user inputed a valid number of items to output
+        if (Int32.Parse(command[2]) > (int)containers[container1Number][1])
+        {
+            Debug.LogError("Cannot output more items than are in the container.");
+            return;
+        }
+
+        // TODO: move arg2 items from arg1 to the output
+        Debug.LogError("OUT command not finsihed.");
         return;
     }
 
